1d72c213d35b0058e7b3405ddc663527ed9ca705
Jitu Khubchandani <kjitu9@gmail.com>
2015-05-22 19:18:46 +0530

    created drupalconasia project version 0.0.1 with ionicframework namespace

---
+++ www/js/app.js
@@ -0,0 +1,30 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'ngCordova'])
+
+.run(function($ionicPlatform, $cordovaSplashscreen) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    setTimeout(function() {
+      $cordovaSplashscreen.hide()
+    }, 5000)
+    var myCountdownTest = new Countdown({
+                  year  : 2016,
+                  month : 4, 
+                  day   : 1,
+                  width : 300, 
+                  height  : 50
+                  });
+
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
